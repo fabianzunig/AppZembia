2590a5a7521f38265fbe01207078daade07e2819
Carolina Gallardo <caritogap@gmail.com>
2019-10-23 17:21:51 -0300

    First draft of Home Screen

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image,Dimensions,Button} from 'react-native';
+const screenHeight=Dimensions.get('window').height
+const screenWidth=Dimensions.get('window').width
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('/home/caritogap/AppZembia/logo.png')} style={{width:screenWidth,resizeMode:'contain',position:'absolute',top:screenHeight/5}}/>
+      <View style={{width: screenWidth/2, height:20, alignSelf:'center',position:'absolute',bottom:screenHeight*2.5/6}}>
+       <Button title="Rendir Gastos" color="black" ></Button>
+      </View>
     </View>
   );
 }
@@ -12,8 +17,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#c03c22',
+
+  },
+  button: {
+
+    color:'#000',
+
   },
 });
